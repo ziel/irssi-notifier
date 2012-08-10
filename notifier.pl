adce61d48b3c94a64a5aae50535f5d106c05632a
Patrick Kontschak <patrick.kontschak@gmail.com>
2012-08-10 09:49:42 -0400

    Finally, differences
    Now we can actually see what has been changed from growl.pl to work as notifier.pl.
    
    Well, nothing much tbh. But it was still necessary to make it visually obvious that we changed the code and not just completely reinvented it.
    
    Signed-off-by: Patrick Kontschak <patrick.kontschak@gmail.com>

--- notifier.pl
+++ notifier.pl
@@ -1,41 +1,33 @@
 # == WHAT
-# Simple script for growl notifications in irssi
+# Simple script for irssi to trigger Mac OS X 10.8's Notification Center
 #
 # == WHO
-# Nate Murray 2008
+# Patrick Kontschak 2012
+# 
+# Forked from Nate Murray's irssi-growl: https://github.com/jashmenn/irssi-growl
 # 
 # == CONFIG
-#   /SET growl_icon [filename]
-#   /SET growl_on_regex [regex]
-#   /SET growl_channel_regex [regex]
+#   /SET notifier_on_regex [regex]
+#   /SET notifier_channel_regex [regex]
 #
 # == EXAMPLES
 #
-#   growl on mynickname
-#   /SET growl_on_regex mynickname
+#   notifier on mynickname
+#   /SET notifier_on_regex mynickname
 #
-#   growl on everything:
-#   /SET growl_on_regex .*
+#   notifier on everything:
+#   /SET notifier_on_regex .*
 #
 #   everything but jdewey
-#   /SET growl_on_regex (?=^(?:(?!jdewey).)*$).*
+#   /SET notifier_on_regex (?=^(?:(?!jdewey).)*$).*
 #
-#   only growl things for mychannel1 and mychannel2
-#   /SET growl_channel_regex (mychannel1|mychannel2)
-# 
-#   enable the icon
-#   /SET growl_icon irssi-flame.png
+#   only notifier things for mychannel1 and mychannel2
+#   /SET notifier_channel_regex (mychannel1|mychannel2)
 # 
 # == INSTALL
-# Place these files in `~/.irssi/scripts/`. Put your growl icon in there too.
-# /script load growl.pl
+# Place notifier.pl in `~/.irssi/scripts/`.
+# /script load notifier.pl
 #
-# == CONTRIBUTE
-# If anyone has a better suggestion to DRY up the signals I would appreciate it. 
-# 
-# http://gist.github.com/6206
-# or 
-# git clone git://gist.github.com/6206.git gist-6206
 
 use strict;
 use Irssi;
@@ -45,34 +37,29 @@ use vars qw($VERSION %IRSSI);
 # Dev. info ^_^
 $VERSION = "0.0";
 %IRSSI = (
-  authors     => "Nate Murray",
-  contact     => "nate\@natemurray.com",
-  name        => "Growl",
-  description => "Simple script that will growlnotify the messages",
+  authors     => "Patrick Kontschak",
+  contact     => "patrick.kontschak\@gmail.com",
+  name        => "Notifier",
+  description => "Simple script that will trigger Mac OS X 10.8's Notification Center",
   license     => "GPL",
-  url         => "http://www.xcombinator.com",
-  changed     => "Mon Sep 22 11:55:07 PDT 2008"
+  url         => "http://www.codinggoat.com",
+  changed     => "Wed  8 Aug 2012 14:40:15 EDT"
 );
 
 # All the works
-sub do_growl {
+sub do_notifier {
   my ($server, $title, $data) = @_;
-  my $icon = growl_locate_icon(Irssi::settings_get_str('growl_icon'));
     $data =~ s/["';]//g;
-    if ($server->{usermode_away}) {
-      system("growlnotify --sticky --image '$icon' -m '$data' -t '$title' >> /dev/null 2>&1");
-    } else {
-      system("growlnotify --image '$icon' -m '$data' -t '$title' >> /dev/null 2>&1");
-    }
+    system("terminal-notifier -message '$data' -title '$title' >> /dev/null 2>&1");
     return 1
 }
 
-sub growl_it {
+sub notifier_it {
   my ($server, $title, $data, $channel, $nick) = @_;
 
-    my $filter = Irssi::settings_get_str('growl_on_regex');
-    my $channel_filter = Irssi::settings_get_str('growl_channel_regex');
-    my $growl_on_nick = Irssi::settings_get_str('growl_on_nick');
+    my $filter = Irssi::settings_get_str('notifier_on_regex');
+    my $channel_filter = Irssi::settings_get_str('notifier_channel_regex');
+    my $notifier_on_nick = Irssi::settings_get_str('notifier_on_nick');
 
     my $current_nick = $server->{nick};
     if($filter) {
@@ -83,47 +70,47 @@ sub growl_it {
     }
 
     $title = $title . " " . $channel;
-    do_growl($server, $title, $data);
+    do_notifier($server, $title, $data);
 }
 
 # All the works
-sub growl_message {
+sub notifier_message {
   my ($server, $data, $nick, $mask, $target) = @_;
-    growl_it($server, $nick, $data, $target, $nick);
+    notifier_it($server, $nick, $data, $target, $nick);
   Irssi::signal_continue($server, $data, $nick, $mask, $target);
 }
 
-sub growl_join {
+sub notifier_join {
   my ($server, $channel, $nick, $address) = @_;
-    growl_it($server, "Join", "$nick has joined", $channel, $nick);
+    notifier_it($server, "Join", "$nick has joined", $channel, $nick);
   Irssi::signal_continue($server, $channel, $nick, $address);
 }
 
-sub growl_part {
+sub notifier_part {
   my ($server, $channel, $nick, $address) = @_;
-    growl_it($server, "Part", "$nick has parted", $channel, $nick);
+    notifier_it($server, "Part", "$nick has parted", $channel, $nick);
   Irssi::signal_continue($server, $channel, $nick, $address);
 }
 
-sub growl_quit {
+sub notifier_quit {
   my ($server, $nick, $address, $reason) = @_;
-    growl_it($server, "Quit", "$nick has quit: $reason", $server, $nick);
+    notifier_it($server, "Quit", "$nick has quit: $reason", $server, $nick);
   Irssi::signal_continue($server, $nick, $address, $reason);
 }
 
-sub growl_invite {
+sub notifier_invite {
   my ($server, $channel, $nick, $address) = @_;
-    growl_it($server, "Invite", "$nick has invited you on $channel", $channel, $nick);
+    notifier_it($server, "Invite", "$nick has invited you on $channel", $channel, $nick);
   Irssi::signal_continue($server, $channel, $address);
 }
 
-sub growl_topic {
+sub notifier_topic {
   my ($server, $channel, $topic, $nick, $address) = @_;
-    growl_it($server, "Topic: $topic", "$nick has changed the topic to $topic on $channel", $channel, $nick);
+    notifier_it($server, "Topic: $topic", "$nick has changed the topic to $topic on $channel", $channel, $nick);
   Irssi::signal_continue($server, $channel, $topic, $nick, $address);
 }
 
-sub growl_privmsg {
+sub notifier_privmsg {
   # $server = server record where the message came
   # $data = the raw data received from server, with PRIVMSGs it is:
   #         "target :text" where target is either your nick or #channel
@@ -131,35 +118,21 @@ sub growl_privmsg {
   # $host = host of the nick who sent the message
   my ($server, $data, $nick, $host) = @_;
     my ($target, $text) = split(/ :/, $data, 2);
-    # growl_it($server, $nick, $data, $target, $nick); # actually, don't do this.
+    # notifier_it($server, $nick, $data, $target, $nick); # actually, don't do this.
   Irssi::signal_continue($server, $data, $nick, $host);
 }
 
-sub growl_locate_icon {
-  # $file = the name of the icon file to look for
-  my ($file) = @_;
-  if (-e "$file") {
-    return "$file";
-  }
-  foreach (@INC) {
-    if (-e "$_/$file") {
-      return "$_/$file";
-    }
-  }
-}
-
 # Hook me up
-Irssi::settings_add_str('misc', 'growl_icon', 'irssi-flame.png');
-Irssi::settings_add_str('misc', 'growl_on_regex', 0);      # false
-Irssi::settings_add_str('misc', 'growl_channel_regex', 0); # false
-Irssi::settings_add_str('misc', 'growl_on_nick', 1);       # true
-Irssi::signal_add('message public', 'growl_message');
-Irssi::signal_add('message private', 'growl_message');
-Irssi::signal_add('message own_public', 'growl_message');
-Irssi::signal_add('message own_private', 'growl_message');
-Irssi::signal_add('message join', 'growl_join');
-Irssi::signal_add('message part', 'growl_part');
-Irssi::signal_add('message quit', 'growl_quit');
-Irssi::signal_add('message invite', 'growl_invite');
-Irssi::signal_add('message topic', 'growl_topic');
-Irssi::signal_add('event privmsg', 'growl_privmsg');
+Irssi::settings_add_str('misc', 'notifier_on_regex', 0);      # false
+Irssi::settings_add_str('misc', 'notifier_channel_regex', 0); # false
+Irssi::settings_add_str('misc', 'notifier_on_nick', 1);       # true
+Irssi::signal_add('message public', 'notifier_message');
+Irssi::signal_add('message private', 'notifier_message');
+Irssi::signal_add('message own_public', 'notifier_message');
+Irssi::signal_add('message own_private', 'notifier_message');
+Irssi::signal_add('message join', 'notifier_join');
+Irssi::signal_add('message part', 'notifier_part');
+Irssi::signal_add('message quit', 'notifier_quit');
+Irssi::signal_add('message invite', 'notifier_invite');
+Irssi::signal_add('message topic', 'notifier_topic');
+Irssi::signal_add('event privmsg', 'notifier_privmsg');
